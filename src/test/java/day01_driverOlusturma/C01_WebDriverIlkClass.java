de9d0d7128b7bc70e20aa6bd2ac608be7369e4d2
Merve Nur Tasan <127309624+mervenurtasann@users.noreply.github.com>
2023-05-02 11:58:05 +0300

    first

--- src/test/java/day01_driverOlusturma/C01_WebDriverIlkClass.java
+++ src/test/java/day01_driverOlusturma/C01_WebDriverIlkClass.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class C01_WebDriverIlkClass {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver_win32/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com.tr/");
